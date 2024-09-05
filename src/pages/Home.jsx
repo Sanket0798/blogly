d8a60ea3d07ea4cd75951e04090c433f64153803
Sanket0798 <rathodsanket2022@gmail.com>
2024-09-05 23:22:13 +0530

    Home section has been modified.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,58 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const posts = [
+    {
+      id: 1,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/1.jpg"),
+    },
+    {
+      id: 2,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/2.jpg"),
+    },
+    {
+      id: 3,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/3.jpg"),
+    },
+    {
+      id: 4,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/4.jpg"),
+    },
+  ];
+
+  return (
+    <div className="home">
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+            </div>
+            <div className="content">
+              <Link to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+                <p>{post.desc}</p>
+                <button>Read More</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;

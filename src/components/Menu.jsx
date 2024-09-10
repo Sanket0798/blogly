18a5b6ccf3d760bbb5317f939445d58e6cd9b285
Sanket0798 <rathodsanket2022@gmail.com>
2024-09-10 12:48:13 +0530

    Menu Page has been developed.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  const posts = [
+    {
+      id: 1,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/1.jpg"),
+    },
+    {
+      id: 2,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/2.jpg"),
+    },
+    {
+      id: 3,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/3.jpg"),
+    },
+    {
+      id: 4,
+      title:
+        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, dolores! A sequi laudantium sit nam?",
+      img: require("../img/home/4.jpg"),
+    },
+  ];
+
+  return (
+    <div className="menu">
+      <h1>Other posts you may like</h1>
+      {posts.map((post) => (
+        <div className="post" key={post.id}>
+          <img src={post.img} alt="" />
+          <h2>{post.title}</h2>
+          <button>Read More</button>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;

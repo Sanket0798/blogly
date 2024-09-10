e538cbbf9abcbbe31e19c34b77763f2bc0507f2f
Sanket0798 <rathodsanket2022@gmail.com>
2024-09-10 12:10:52 +0530

    Single Page has been developed.

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Menu from "../components/Menu";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={require("../img/single/img1.jpg")} alt="" />
+        <div className="user">
+          <img src={require("../img/single/userImg.jpg")} alt="" />
+          <div className="info">
+            <span>Lara</span>
+            <p className="postDate">Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={require("../img/edit.png")} alt="" />
+            </Link>
+            <img src={require("../img/delete.png")} alt="" />
+          </div>
+        </div>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+          molestias necessitatibus ab voluptatum amet quod. Dignissimos, neque?
+          Unde, velit rem!
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita
+          quidem quae dignissimos! Quae quisquam quaerat dolore provident quia
+          atque laudantium suscipit maxime, fugiat, ex facilis aliquid velit
+          doloribus ipsam earum ratione quo recusandae cumque dolorum culpa
+          officia iure ad harum. Nisi, recusandae ipsam! Nostrum cum tenetur
+          labore, praesentium magnam molestias cumque, deleniti soluta tempore
+          excepturi dolorum inventore at ex reprehenderit modi unde libero.
+          Ipsam illum nostrum excepturi aspernatur! Eaque voluptas velit nam aut
+          libero omnis, aliquam placeat consequatur similique provident
+          veritatis minima impedit nobis quaerat, quam debitis id, eligendi
+          porro nihil quisquam tenetur in quos rem. Voluptatum impedit
+          consequuntur similique?
+        </p>
+      </div>
+      <Menu/>
+    </div>
+  );
 };
 
 export default Single;

c58be2d1d78fd0d056f9f27f3358932dd650878c
Sanket0798 <rathodsanket2022@gmail.com>
2024-09-11 10:52:30 +0530

    Menu Page has been developed.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -34,11 +34,11 @@ const Menu = () => {
 
   return (
     <div className="menu">
-      <h1>Other posts you may like</h1>
+      <h1 className="otherPost">Other posts you may like</h1>
       {posts.map((post) => (
         <div className="post" key={post.id}>
           <img src={post.img} alt="" />
-          <h2>{post.title}</h2>
+          <h2 className="otherPostTitle">{post.title}</h2>
           <button>Read More</button>
         </div>
       ))}

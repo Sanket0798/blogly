eede3bf131d1dffa848e16a3582b735be4876de9
Sanket0798 <rathodsanket2022@gmail.com>
2024-09-25 12:39:08 +0530

    Minor Changes.

--- api/db.js
+++ api/db.js
@@ -1,15 +1,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  // host: "localhost",
-  // user: "root",
-  // password: "Sanket@123",
-  // database: "blog",
-  // insecureAuth: true,
-
   host: "localhost",
   user: "root",
   password: "password",
   database: "blog",
-  // port: 3306,
 });

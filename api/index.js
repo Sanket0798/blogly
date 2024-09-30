304d5fcc5d604ab09a05462435e10cf7afca2825
Sanket0798 <rathodsanket2022@gmail.com>
2024-09-30 11:45:16 +0530

    cors has been added.

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ import express from "express";
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors);
 
 app.use(express.json());
 app.use("/api/auth", authRoutes);
